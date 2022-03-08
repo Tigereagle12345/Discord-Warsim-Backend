71e92aa854d6de9a40fdb3e10f0d6ea14673a239
Teresa [Redacted] <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-08 13:31:36 -0500

    Begun adapting warsim
    Begun adapting Warsim for Discord

--- README.md
+++ README.md
@@ -1,26 +1,20 @@
-# Discount Warsim
-  
-Discount Warsim is exactly what it sounds like, a discount version of Warsim: Realm of Aslona that I am making.  
-It serves no purpose what so ever except your entertainment (and my suffering).  
-I know I know, I shouldn't have made this in python but I don't give a fuck.  
+# Discord Warsim
+###### Just the backend tho
+
+Discord Warsim, Discount Warsim but on Discord  
+Bottom text
 
 # TODO list
 
- - [ ] Fix save creation
- - [ ] Add loading
- - [ ] Add saving
+ - [ ] Add user process
+    - [ ] Determine how to handle saves
+    - [ ] Create save system
  - [ ] Make the game playable
-    - [ ] Finish the math behind battles
-    - [ ] Start making systems to handle wars and diplomacy and stuff
-        - [ ] Wars
-        - [ ] Diplomacy
-    - [ ] Start work on internal player relations systems
-- [ ] Make the game look good
-    - [ ] Fancy formating on text
-    - [ ] Add ACSII art
-    - [ ] Add country names
-        - [ ] Player
-        - [ ] CPU
+    - [ ] Market
+        - [ ] Research result and weapon purchases
+    - [ ] Research and weapons development
+    - [ ] Public Opinion
+    - [ ] XP and levels
 - [ ] Add in some easter eggs
-    - [x] Normal easter eggs
+    - [ ] Normal easter eggs
     - [ ] Hidden trans pride flags
--- src/warsim.py
+++ src/warsim.py
@@ -5,7 +5,7 @@ import menus
 import calc
 
 class country:
-    def __init__(self, ID, GOLD, LAND, ESOLDIER, SOLDIER, PSOLDIER, TECH, WAR, ISPLY, NAME):
+    def __init__(self, ID, GOLD, LAND, ESOLDIER, SOLDIER, PSOLDIER, TECH, WAR, ISPLY, NAME, LEVEL, XP):
         self.id = ID
         self.gold = GOLD
         self.land = LAND
@@ -17,30 +17,5 @@ class country:
         self.warlike = WAR
         self.isPlayer = ISPLY
         self.name = NAME
-    def battle(self, type, isVIC, stakes, outcome):
-        if isVIC == 1:
-            ### Type one is a raid, type two is a skirmish, type three is a full blown invasion
-            if type == 1:
-                ### Loss
-                if outcome == 1:
-                    self.gold -= stakes
-                ### Victory
-                else:
-                    vict = 'yes'
-                    ### Defensive victories don't have rewards
-            elif type == 3:
-                if outcome == 1:
-                    self.land -= 1
-                ### Victory
-                else:
-                    vict = 'yes'
-                    ### Defensive victories don't have rewards
-        elif isVIC == 0:
-            if type == 3:
-                if outcome == 0:
-                    self.land += stakes
-                else:
-                    deft = 'yes'
-            elif type == 1:
-                if outcome == 0:
-                    self.gold += stakes
+        self.level = LEVEL
+        self.xp = XP
