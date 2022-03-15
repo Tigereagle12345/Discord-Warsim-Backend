359f81c7701f3bf6ef0e4243b38ccb1980d1e42e
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-15 11:27:49 -0400

    Added class levels
    Added class levels, which handles level calculations.

--- src/calc.py
+++ src/calc.py
@@ -5,3 +5,23 @@ class warscore:
     perSoldier : int
     perElite : int
     perPeasant : int
+class levels:
+    def __init__(self, xp):
+        self.xp = xp
+    def levelCalc(self, max = 300):
+        self.levels = [1000]
+        self.max = max
+        for i in range(max):
+            self.levels.append(self.levels[i] * 2)
+        for i in range(len(self.levels)):
+            if self.xp >= self.levels[i]:
+                if not i + 1 == len(self.levels) and self.xp < self.levels[i + 1]:
+                    break
+                elif i + 1 == len(self.levels):
+                    break
+                else:
+                    pass
+            elif i == 0:
+                break
+        self.level = i + 1
+        return self.level
