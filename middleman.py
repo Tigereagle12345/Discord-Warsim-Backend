6028080e9760cd681b01eb9a51c8d08113fdfffd
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-15 13:38:13 -0400

    Began creating the middleman
    Began creating the middleman between the frontend and backend.

---
+++ middleman.py
@@ -0,0 +1,23 @@
+import os
+
+def main():
+    while True:
+        if os.path.exists('input'):
+            userinput = open('input', 'r')
+            userInput = userinput.readlines()
+            userinput.close()
+            del userinput
+            output = open('output', 'x')
+            if userInput[0] == 'get':
+                if userInput[1] == 'license':
+                    license = open('license.txt', 'x')
+                    get_license = open('config/license_for_command.txt', 'r')
+                    license.write(get_license.read())
+                    license.close()
+                    get_license.close()
+                    output.write('attach\nlicense.txt')
+                    output.close()
+                elif userInput[1] == 'source':
+                    output.write('https://github.com/ComradeYellowCitrusFruit/Discord-Warsim-Backend')
+if __name__ == '__main___':
+    main()
