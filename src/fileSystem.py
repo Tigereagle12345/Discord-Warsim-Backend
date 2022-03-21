f225cf7a22db5e672076a3ab329690f89eb38cd2
Villavicencio <teresaavillavicencio@gmail.com>
2022-03-21 13:24:23 -0400

    Update fileSystem.py

--- src/fileSystem.py
+++ src/fileSystem.py
@@ -8,4 +8,5 @@ from base64 import b64encode, b64decode
 from misc import *
 
 def newUser(id : str, choice : int):
-    
+    userDatabase = sqlite3.connect('users.db')
+    userCursor = userDatabase.cursor()
