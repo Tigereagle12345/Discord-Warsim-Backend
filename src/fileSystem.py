5796e29aa889a697b83bb963fd92f26444e61ded
Villavicencio <teresaavillavicencio@gmail.com>
2022-03-21 14:03:47 -0400

    Update fileSystem.py

--- src/fileSystem.py
+++ src/fileSystem.py
@@ -7,6 +7,16 @@ import sqlite3
 from base64 import b64encode, b64decode
 from misc import *
 
-def newUser(id : str, choice : int):
+def newUser(id : str):
     userDatabase = sqlite3.connect('users.db')
+    userDatabase.row_factory = sqlite3.Row
     userCursor = userDatabase.cursor()
+    userCursor.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users';''')
+    if not userCursor.fetchone()[0]==1:
+        userCursor.execute('CREATE TABLE users (id int, gold int, level int, xp int)')
+    gold = random.randrange(1000,25000)
+    insert = id + ',' + str(gold) + ',0,0);'
+    userCursor.execute('INSERT INTO users (id,gold,level,xp) VALUES (' + insert)
+    userCursor.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='land';''')
+    if not userCursor.fetchone()[0]==1:
+        userCursor.execute('CREATE TABLE land (ownerid int, barren bool, population int, infected int, type int, plague text)')
